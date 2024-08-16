2cb06c519da9dbc23e82fe044c9e7d895088a5d5
vkashegde <vikas@kofluence.com>
2024-08-16 23:31:03 +0530

    test commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
